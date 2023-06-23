c4c8360551a7c7da2d9c49c7fd49df27f389df71
Sumit S Chawla <sam.chawla26@gmail.com>
2023-06-23 16:26:53 +0530

    App method params added
    - Added requirements.txt
    - Learnt Status Codes, Tags, Summary and description

--- fastapi_dummy/main.py
+++ fastapi_dummy/main.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Optional
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Response, status
 
 app = FastAPI()
 
@@ -11,7 +11,13 @@ class Status(str, Enum):
     inactive = "InActive"
 
 
-@app.get("/")
+@app.get(
+    "/",
+    tags=["home"],
+    summary="Home Page",
+    description="This is a home page",
+    response_description="Returns Home Page",
+)
 def index():
     """Base Endpoint
 
@@ -21,7 +27,7 @@ def index():
     return {"message": "Hello World"}
 
 
-@app.post("/")
+@app.post("/", tags=["home"])
 def index2():
     """Base Endpoint with post
 
@@ -31,7 +37,12 @@ def index2():
     return {"message": "Hello World"}
 
 
-@app.get("/blogs/")
+@app.get(
+    "/blogs/",
+    tags=["blog", "list"],
+    summary="Fetch all blogs",
+    description="Replicated the functionality of fetching blogs",
+)
 def blogs():
     """Returns Blog content
 
@@ -42,24 +53,31 @@ def blogs():
 
 
 # Path Params
-@app.get("/blogs/{id}")
-def blog(id: int):
+@app.get("/blogs/{id}", status_code=status.HTTP_200_OK, tags=["blog"])
+def blog(id: int, response: Response):
     """Returns Blog content
 
+    - **id** mandatory path parameter
+
     Returns:
-        json: blog content
+        json: blog data
     """
-    return {"blog_id": id, "content": "This is a sample Blog"}
+    if id > 10:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"error": "Content not available"}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {"blog_id": id, "content": "This is a sample Blog"}
 
 
 # PreDefined Values
-@app.get("/user/status/{status}")
+@app.get("/user/status/{status}", tags=["user"])
 def user_type(status: Status):
     return "Status is : {status}"
 
 
 # Query Params with Default values
-@app.get("/blogs/")
+@app.get("paginated/blogs/", tags=["paginated"])
 def get_blogs(page=1, page_size=10):
     data = []
     for val in range(1, int(page_size) + 1):
@@ -68,7 +86,7 @@ def get_blogs(page=1, page_size=10):
 
 
 # Query Params with Optional values
-@app.get("/blogs/optional/")
+@app.get("/blogs/optional/", tags=["blog"])
 def get_blogs_optional(page=1, page_size=Query(default=10)):
     data = []
     for val in range(1, int(page_size) + 1):
